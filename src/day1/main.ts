fa07e9f129f0d995bb812d3a0738f01e855bf4f5
Simon Cobb <simoncobb@msn.com>
2022-12-16 23:36:41 +0000

    read in input

---
+++ src/day1/main.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+
+
+function parseOutput(input: string) : number {
+    if(input == ''){
+        return -1
+    }
+    return parseInt(input);
+}
+
+function splitBasedOnValue<Type>(values : Array<Type>, splitValue : Type): Array<Array<Type>>{
+    let listOfLists: Type[][]  = []
+    let list: Array<Type> = []
+    values.forEach( (x : Type) => {
+        if(x == splitValue){
+            listOfLists.push(list)
+            list = []
+        
+        } else{
+        list.push(x)
+        }
+    })
+    return listOfLists
+    }
+
+function readInput(filepath: string): number[][]{
+    let file = readFileSync(filepath,'utf-8')
+    let allCalories: number[] = file.split(/\r?\n/).map(parseOutput)
+    return splitBasedOnValue(allCalories, -1)
+}
+
+console.log(readInput('day1/test.txt'))
