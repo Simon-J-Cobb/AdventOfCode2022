853d3afedb4ac0acc4f0aabf65af40424884c225
Simon Cobb <simoncobb@msn.com>
2022-12-17 19:07:14 +0000

    day4 complete

--- src/day4/main.ts
+++ src/day4/main.ts
@@ -25,4 +25,29 @@ function solutionOne(filepath:string): number {
 }
 
 console.log(solutionOne('resources/day4/test.txt'))
+console.log(solutionOne('resources/day4/input.txt'))
+
+function overlaps(pairs: number[][]){
+    if(contains(pairs)){
+        return contains(pairs)
+    } else{
+        if(pairs[1][0] <= pairs[0][0] && pairs[0][0] <= pairs[1][1]){
+            return 1
+        } else if(pairs[1][0] <= pairs[0][1] && pairs[0][1] <= pairs[1][1]){
+            return 1
+        } else {
+            return 0
+        }
+
+    }
+
+}
+
+function solutionTwo(filepath:string): number {
+    return readInput(filepath).map((x) => overlaps(x)).reduce((x,y) => x + y)
+}
+
+console.log(solutionTwo('resources/day4/test.txt'))
+console.log(solutionTwo('resources/day4/input.txt'))
+
 
