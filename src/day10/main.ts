fa72d9b234c2c381d43b759f418b962f446cfd88
Simon Cobb <simoncobb@msn.com>
2022-12-19 13:54:47 +0000

    working part one and test part two for day 10

--- src/day10/main.ts
+++ src/day10/main.ts
@@ -57,3 +57,65 @@ function solutionOne(filepath:string){
 }
 
 console.log(solutionOne('resources/day10/testTwo.txt'))
+console.log(solutionOne('resources/day10/input.txt'))
+
+interface crtPosition{
+    row: number,
+    column: number
+}
+
+function crtPositionAtCycle(computerState: computerState){
+    return {row: Math.floor(computerState.cycle / 40) % 6, column: computerState.cycle % 40 }
+}
+
+function pixelLights(computer: computerScreen){
+    let computerState = computer.state
+    let crtPosition = crtPositionAtCycle(computerState)
+    //console.log(crtPosition)
+    //console.log(computerState.x)
+    if(crtPosition.column == computerState.x || crtPosition.column == computerState.x + 1 || crtPosition.column == computerState.x - 1){
+        computer.grid[crtPosition.row][crtPosition.column] = '#'
+        //console.log(computer.grid[crtPosition.row])
+    } else{
+        return
+    }
+}
+
+type pixel =  '#' | '.'
+
+interface computerScreen{
+    state: computerState,
+    grid: pixel[][]
+
+}
+
+function screencycle(computer: computerScreen, instruction: instruction): computerScreen{
+    let state = computer.state
+    if(instruction.direction == 'noop'){
+        pixelLights(computer)
+        state.cycle += 1
+        return computer
+    } else if(instruction.direction == 'addx'){
+        pixelLights(computer)
+        state.cycle += 1
+        pixelLights(computer)
+        state.cycle += 1
+        state.x += instruction.amount as number
+        
+    }
+    return computer
+}
+
+function solutionTwo(filepath:string){
+    let instructions = readInput(filepath)
+    let grid: pixel [][] = Array(6).fill([]).map(function(){return Array(40).fill('.')})
+    let computer = {state: {x:1,cycle:0}, grid: grid }
+    for(let instruction of instructions){
+        screencycle(computer,instruction)
+    }
+    return computer.grid
+}
+for(let line of solutionTwo('resources/day10/testTwo.txt').map((x
+) => x.join(''))){
+    console.log(line)
+}
