4d79ad139724a121e6a4aaeaf7ba325ce09070cb
Simon Cobb <simoncobb@msn.com>
2022-12-20 10:27:06 +0000

    day12 complete

--- src/day12/main.ts
+++ src/day12/main.ts
@@ -50,14 +50,16 @@ function reachable(start: node, end: node | undefined) {
 
 }
 
-function Dijkstra(graph: node[]) {
+function Dijkstra(graph: node[], startNode: node) {
     let current = graph[0]
     for (let node of graph) {
-        if (node.value == 'S') {
+        if (node == startNode) {
             node.distance = 0
+            node.visited = false
             current = node
         } else {
             node.distance = Infinity
+            node.visited = false
 
         }
     }
@@ -78,10 +80,31 @@ function Dijkstra(graph: node[]) {
             return current.distance
         }
         current = graph.filter((x) => !x.visited).reduce(function (x, y) { return (x && x.distance < y.distance) ? x : y; })
+        if(current.distance == Infinity){
+            return Infinity
+        }
     }
 }
 
+function solutionOne(filepath: string) {
+    let graph = readInput(filepath)
+    let startnode = graph.filter((x) => x.value == 'S')[0]
+    return Dijkstra(graph, startnode)
+}
 
 console.log(readInput('resources/day12/test.txt'))
-console.log(Dijkstra(readInput('resources/day12/test.txt')))
-console.log(Dijkstra(readInput('resources/day12/input.txt')))
+console.log(solutionOne('resources/day12/test.txt'))
+console.log(solutionOne('resources/day12/input.txt'))
+
+function solutionTwo(filepath: string) {
+    let graph = readInput(filepath)
+    let startnodes = graph.filter((x) => x.value == 'a' || x.value == 'S')
+    let distances = []
+    for (let node of startnodes) {
+        distances.push(Dijkstra(graph, node))
+    }
+    return distances.reduce(function (x, y) { return (x && x < y) ? x : y; })
+}
+
+console.log(solutionTwo('resources/day12/test.txt'))
+console.log(solutionTwo('resources/day12/input.txt'))
