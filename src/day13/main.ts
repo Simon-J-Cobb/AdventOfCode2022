85d9c0c0196185823c689da7ab085a69ed32ca34
Simon Cobb <simoncobb@msn.com>
2022-12-20 14:14:01 +0000

    test passing day 13

--- src/day13/main.ts
+++ src/day13/main.ts
@@ -1,12 +1,62 @@
-import { mainModule } from "process"
+import { readFileSync } from "fs"
 
-interface packet{
-    value : number | Array<Number>
+interface packet {
+    value: number | Array<any>
 }
 
-export function compare(packetOne: packet, packetTwo: packet){
-    if(typeof packetOne.value == 'number' && typeof packetTwo.value == 'number'){
+function readInput(filepath: string): packet[][]{
+    let file = readFileSync(filepath, 'utf-8')
+    let lines = file.split(/\r?\n/)
+    const chunkSize = 3;
+    let packets = []
+    for (let i = 0; i < lines.length; i += chunkSize) {
+        packets.push([{value: JSON.parse(lines[i])} as packet,{value: JSON.parse(lines[i+1])} as packet])
+
+    }
+    return packets
+    
+}
+
+export function compare(packetOne: packet, packetTwo: packet): boolean {
+    if (typeof packetOne.value == 'number' && typeof packetTwo.value == 'number') {
         return packetOne.value < packetTwo.value
     }
+    else if (typeof packetOne.value == 'object' && typeof packetTwo.value == 'object') {
+        for (let i = 0; i < packetTwo.value.length; i++) {
+            if (i >= packetOne.value.length) {
+                return true
+            } else {
+                if (JSON.stringify(packetOne.value[i]) === JSON.stringify(packetTwo.value[i])) {
+                    continue
+                }
+                return compare({ value: packetOne.value[i] }, { value: packetTwo.value[i] })
+            }
+        }
+    } else if (typeof packetOne.value == 'number' && typeof packetTwo.value == 'object') {
+        return compare({ value: [packetOne.value] }, packetTwo)
+    }
+    else if (typeof packetOne.value == 'object' && typeof packetTwo.value == 'number') {
+        return compare(packetOne, { value: [packetTwo.value] })
+
+    } else{
+        return false
+    }
+    return false
+
 }
+
+function solutionOne(filepath:string){
+    let packets = readInput(filepath)
+    let i = 1
+    let answer = 0
+    for(let packet of packets){
+        if(compare(packet[0],packet[1])){
+            answer += i
+        }
+        i++
+    }
+    return answer
+}
+
+console.log(solutionOne('resources/day13/test.txt'))
 
--- test/day13/main.test.ts
+++ test/day13/main.test.ts
@@ -7,3 +7,15 @@ test('both values are integers', () => {
 test('both values are integers', () => {
     expect(compare({ value: 2 }, { value: 1 })).toBe(false)
 })
+
+test('compare two lists', () => {
+    expect(compare({ value: [2, 1] }, { value: [2, 2] })).toBe(true)
+})
+
+test('compare two lists', () => {
+    expect(compare({ value: [2, 1] }, { value: [1, 1] })).toBe(false)
+})
+
+test('compare two lists', () => {
+    expect(compare({ value: [[1],[2,3,4]] }, { value: [[1],4] })).toBe(true)
+})
